2c480907ff850c2e5bd766e3f00f667d4990ff39
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-06-01 07:47:03 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Django Lang train
---
+++ post/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.title
